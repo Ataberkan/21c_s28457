a001531d974521d28a5dbe70d7d6eb0e6ec15bf8
Ataberkan <ataberkangazi132@gmail.com>
2024-03-21 02:03:30 +0100

    Task 2 was completed

--- lab_3/main.py
+++ lab_3/main.py
@@ -1,3 +1,9 @@
 # Task 1 #
 squares = [x**2 for x in range(1, 11)]
 print(squares)
+
+# Task 2 #
+def generate_squares(start, end):
+    return [x**2 for x in range(start, end + 1)]
+
+print(generate_squares(1, 10))
