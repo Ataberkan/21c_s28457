112dc5e3f01caed2160a3dc434d18a8c6c54c9a4
Ataberkan <ataberkangazi132@gmail.com>
2024-03-21 02:07:44 +0100

    Task 4 was completed

--- lab_3/main.py
+++ lab_3/main.py
@@ -1,3 +1,5 @@
+import math
+
 # Task 1 #
 squares = [x**2 for x in range(1, 11)]
 print(squares)
@@ -18,3 +20,22 @@ class SquareGenerator:
 # Example usage
 generator = SquareGenerator()
 print(generator.generate_squares(1, 10))
+
+# Task 4 #
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        if end < start:
+            raise ValueError("The end value cannot be less than the start value.")
+        return [x ** 2 for x in range(start, end + 1)]
+
+    def calculate_square_roots(self, numbers_list):
+        return [math.sqrt(number) for number in numbers_list]
+
+
+# Example usage
+generator = SquareGenerator()
+squares = generator.generate_squares(1, 10)
+square_roots = generator.calculate_square_roots(squares)
+print("Squares:", squares)
+print("Square Roots:", square_roots)
+
