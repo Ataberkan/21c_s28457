eedf5855cdba2e59439d5cb6fcb045c1fd70681a
Ataberkan <ataberkangazi132@gmail.com>
2024-03-21 02:06:11 +0100

    Task 3 was completed

--- lab_3/main.py
+++ lab_3/main.py
@@ -7,3 +7,14 @@ def generate_squares(start, end):
     return [x**2 for x in range(start, end + 1)]
 
 print(generate_squares(1, 10))
+
+# Task 3 #
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        if end < start:
+            raise ValueError("The end value cannot be less than the start value.")
+        return [x**2 for x in range(start, end + 1)]
+
+# Example usage
+generator = SquareGenerator()
+print(generator.generate_squares(1, 10))
